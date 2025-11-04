bc37b9d826f7456f3339941f103adddc5713b51c
Hinna04 <thuhienchymte932k4@gmail.com>
2025-11-05 02:37:44 +0700

    cập nhật chức năng tìm kiếm

---
+++ Controllers/ServiceController.cs
@@ -0,0 +1,41 @@
+﻿using BTL.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BTL.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ServiceController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /Service?keyword=bún bò
+        public IActionResult Index(string? keyword)
+        {
+            var query = _context.Services
+                .Include(s => s.Category)
+                .Include(s => s.User)
+                .AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(keyword))
+            {
+                keyword = keyword.Trim();
+                query = query.Where(s =>
+                    EF.Functions.Like(s.ItemName, $"%{keyword}%") ||
+                    EF.Functions.Like(s.Description, $"%{keyword}%") ||
+                    EF.Functions.Like(s.Address, $"%{keyword}%"));
+            }
+
+            var results = query
+                .OrderByDescending(s => s.CreateAt)
+                .ToList();
+
+            ViewBag.Keyword = keyword;
+            return View(results);
+        }
+    }
+}
--- Models/Service.cs
+++ Models/Service.cs
@@ -1,22 +1,33 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace BTL.Models
 {
+    [Table("tblService")] 
     public class Service
     {
         [Key]
         public int ItemID { get; set; }
+
+        [Required]
         public string ItemName { get; set; }
+
         public string? Description { get; set; }
+
         public int CategoryID { get; set; }
+
         public int UserID { get; set; }
+
         public string Address { get; set; }
+
         public double? AvgRating { get; set; }
+
+        public string? ImageUrl { get; set; } 
+
         public DateTime CreateAt { get; set; } = DateTime.Now;
 
         // Navigation
         public Category? Category { get; set; }
         public User? User { get; set; }
-        //public ICollection<Review>? Reviews { get; set; }
     }
 }
