e3423535dc641fe63ef5b768f3ae1942de4213a2
hoancong8 <conghoan1182004@gmail.com>
2025-11-06 00:29:54 +0700

    lai

--- Models/ViewModels/ReviewViewModel.cs
+++ Models/ViewModels/ReviewViewModel.cs
@@ -9,10 +9,10 @@ namespace BTL.Models.ViewModels
         public string Title { get; set; }
         public string Content { get; set; }
         public int Rating { get; set; }
-        [Required(ErrorMessage = "Vui lòng nhập mã VerifyKey.")]
-        [StringLength(10, MinimumLength = 10, ErrorMessage = "VerifyKey phải có đúng 10 ký tự.")]
-        [RegularExpression(@"^[0-9][A-Za-z0-9]{9}$", ErrorMessage = "VerifyKey phải bắt đầu bằng số và có 10 ký tự.")]
-        public string VerifyKey { get; set; }
+        //[Required(ErrorMessage = "Vui lòng nhập mã VerifyKey.")]
+        //[StringLength(10, MinimumLength = 10, ErrorMessage = "VerifyKey phải có đúng 10 ký tự.")]
+        //[RegularExpression(@"^[0-9][A-Za-z0-9]{9}$", ErrorMessage = "VerifyKey phải bắt đầu bằng số và có 10 ký tự.")]
+        //public string VerifyKey { get; set; }
         public List<Service> Services { get; set; }  // Danh sách dịch vụ để chọn
     }
 }
